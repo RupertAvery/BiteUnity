f9eebc255edbf03c2aea99a2fb00af802f48ce44
David Khristepher Santos <rupert.avery@gmail.com>
2022-04-13 08:02:28 +0800

    Thread test

--- Assets/BiteUnity/BiteMoveGameObject.cs
+++ Assets/BiteUnity/BiteMoveGameObject.cs
@@ -3,7 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Bite.Compiler;
+using Bite.Modules.Callables;
+using Bite.Runtime;
 using Bite.Runtime.CodeGen;
 using UnityEngine;
 using UnityEngine.Scripting;
@@ -17,10 +21,22 @@ public class BiteMoveGameObject : MonoBehaviour
     private Transform m_TransformToMove;
 
     public Vector3 m_Vector3;
-    private BiteProgram program; 
+    private BiteProgram program;
+    private BiteVm vm = new BiteVm();
+
     void Start()
     {
-        IEnumerable < string > files = Directory.EnumerateFiles(
+        vm.InitVm();
+        vm.RegisterSystemModuleCallables();
+        vm.SynchronizationContext = SynchronizationContext.Current;
+        vm.RegisterExternalGlobalObjects(new Dictionary<string, object>()
+            {
+                { "GameObjectFromUnity", m_GameObjectToMove },
+                { "TransformToUse", m_TransformToMove },
+                { "Vector3", m_Vector3 }
+            });
+
+        IEnumerable< string > files = Directory.EnumerateFiles(
             "Assets\\Bite\\TestMoveGameObject",
             "*.bite",
             SearchOption.AllDirectories );
@@ -28,17 +44,15 @@ public class BiteMoveGameObject : MonoBehaviour
         BiteCompiler compiler = new BiteCompiler();
 
         program = compiler.Compile(files.Select(File.ReadAllText));
-        
-       
+
+        Task.Run(() =>
+        {
+            vm.Interpret(program);
+        });       
     }
 
     private void Update()
     {
-        program.Run(new Dictionary < string, object >()
-        {
-            { "GameObjectFromUnity", m_GameObjectToMove },
-            { "TransformToUse", m_TransformToMove },
-            { "Vector3", m_Vector3 }
-        });
+
     }
 }
